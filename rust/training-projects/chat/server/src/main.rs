76f23d1228625adb4e48cd5be3c2d5c2602f7ba4
joasquintero <joasquinteroc@gmail.com>
2022-07-05 02:40:34 -0400

    [ADD] chat

--- rust/training-projects/chat/server/src/main.rs
+++ rust/training-projects/chat/server/src/main.rs
@@ -3,7 +3,7 @@ use std::net::TcpListener;
 use std::sync::mpsc;
 use std::thread;
 
-const LOCAL: &str = "127.0.0.1:6000";
+const LOCAL: &str = "127.0.0.1:5566";
 const MSG_SIZE: usize = 32;
 
 fn sleep() {
@@ -32,6 +32,7 @@ fn main() {
                         let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
                         let msg = String::from_utf8(msg).expect("Invalid utf8 message");
                         println!("{}: {:?}", addr, msg);
+                        tx.send(msg).expect("Failed to send msg to rx");
                     }
                     Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
                     Err(_) => {
@@ -41,5 +42,16 @@ fn main() {
                 }
             });
         }
+        if let Ok(msg) = rx.try_recv() {
+            clients = clients
+                .into_iter()
+                .filter_map(|mut client| {
+                    let mut buff = msg.clone().into_bytes();
+                    buff.resize(MSG_SIZE, 0);
+                    client.write_all(&buff).map(|_| client).ok()
+                })
+                .collect::<Vec<_>>();
+        }
+        sleep();
     }
 }

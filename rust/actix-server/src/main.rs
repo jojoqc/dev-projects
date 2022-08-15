060711720148e4bf8a9f458072761239ca529d0a
proxyhen <joasquinteroc@gmail.com>
2022-08-14 23:51:16 -0400

    [FIX]

--- rust/actix-server/src/main.rs
+++ rust/actix-server/src/main.rs
@@ -5,6 +5,26 @@ async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
 }
 
+#[get("/status")]
+async fn get_status()-> impl Responder{
+    HttpResponse::Ok().body("get status");
+}
+#[post("/status")]
+async fn post_status()-> impl Responder{
+    HttpResponse::Ok().body("post status");
+}
+#[put("/status/:id")]
+async fn put_status()-> impl Responder{
+    HttpResponse::Ok().body("put status");
+}
+#[delete("/status/:id")]
+async fn delete_status()-> impl Responder{
+    HttpResponse::Ok().body("delete status");
+}
+
+
+
+
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {

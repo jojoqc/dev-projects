54e43a9a7a1b667b00f7830bf3e4bbe9ec9a2d93
joasquintero <joasquinteroc@gmail.com>
2023-02-01 22:28:10 -0400

    [ADD]

--- rust/rusty-blas/src/structs.rs
+++ rust/rusty-blas/src/structs.rs
@@ -1,6 +1,7 @@
 //LEVEL-1V opetarions on vectors
 struct _addv{}
 struct _amaxv{}
+//a*x + y
 struct _axpyv{}
 struct _copyv{}
 struct _dotv{}
@@ -33,3 +34,37 @@ struct _scalm{}
 struct _scal2m{}
 struct _setm{}
 struct _subm{}
+
+
+//Sum of vector magnitudes
+struct asum{}
+//Scalar-vector product
+struct axpy{}
+//Copy vector
+struct copyv{}
+//Dot product
+struct dotv{}
+//Dot product with double precision
+struct sdsdot{}
+//Dot product conjugated
+struct dotc{}
+//Dot product unconjugated
+struct dotu{}
+//Vector 2-norm (Euclidean norm)
+struct nrm2{}
+//Plane rotation of points
+struct rot{}
+//Generate Givens rotation of points
+struct rotg{}
+//Modified Givens plane rotation of points
+struct rotm{}
+//Generate modified Givens plane rotation of points
+struct rotmg{}
+//Vector-scalar products
+struct scal{}
+//Vector-vector swap
+struct swap{}
+//Index of the maximum absolute value element of a vector
+struct iamax{}
+//Index of the minimum absolute value element of a vector
+struct iamin{}

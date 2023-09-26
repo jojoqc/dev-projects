44d9ad625a33537a2f6b305af35b1e953d188b85
joasquintero <joasquinteroc@gmail.com>
2023-09-26 11:30:33 -0400

    [WIP] sketch or something

--- rust/fsm/src/main.rs
+++ rust/fsm/src/main.rs
@@ -1,6 +1,3 @@
-#[allow(non_camel_case_types)]
-//#[derive(Debug)]
-//#[derive(Copy, Clone)]
 enum eStates{
     Halt,
     Error,
@@ -11,23 +8,10 @@ enum eStates{
     Previous,
     Jump,
 }
-#[allow(non_camel_case_types)]
-//#[derive(Debug)]
-//#[derive(Copy, Clone)]
 struct sMachine{
     state: eStates,
     uuid: u16,
 }
-
-//#[allow(non_camel_case_types)]
-//trait tStates{
-//    fn new()->Self;
-//    fn status(&self)->Self;
-    //fn set(&self)->Self; 
-//}
-
-#[allow(non_camel_case_types)]
-//impl tStates for sMachine{
 impl sMachine {
     fn new()->Self{
         sMachine{
